2457398eca596bf6d3e9e638b512eb2b7f8d62ca
Sebastien L. F <slf.pro@protonmail.com>
2019-07-05 19:34:37 +0200

    Add call to Codewars API.

--- src/ChallengeContainer.js
+++ src/ChallengeContainer.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import Challenge from './Challenge.js'
 
-function ChallengeContainer () {
+import codewars_APIKEY from './codewars_APIKEY.js'
 
-  var topicsChosen = [];
+class ChallengeContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { challenges: {} }
+  }
 
-  return (
-    <div><Challenge url='https://reactjs.org/docs/state-and-lifecycle.html' /></div>
-  )
+  componentDidMount() {
+
+    const url = 'https://www.codewars.com/api/v1/code-challenges/';
+    let challenge_code = '57cc847e58a06b1492000264?';
+    fetch(url + challenge_code + 'access_key=' + codewars_APIKEY)
+    .then( (response) => response.json() )
+    .then( function(myJson) {
+      console.log(myJson);
+      console.log(JSON.stringify(myJson));
+    })
+  }
+
+  render() {
+    return <div>retour</div>
+  }
+
 }
 
 export default ChallengeContainer
