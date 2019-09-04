5f63d86d58dd1f737a98634e24f4aba0b6829c75
zelol <slf.pro@gmail.com>
2019-09-05 00:14:10 +0200

    add styling object in component

--- src/ChallengeContainer.js
+++ src/ChallengeContainer.js
@@ -2,24 +2,41 @@ import React from 'react';
 import Challenge from './Challenge.js'
 import { generateSerie } from './topics.js'
 
+const style = {
+  width: '800px',
+  height: '500px',
+  border: 0,
+  borderRadius: '4px',
+  overflow: 'hidden',
+  margin: '0 auto'
+}
 
 class ChallengeContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       challenges: [
-        {id: "19078", description: "Rewrite reduce function.", initialCode: "function reduce() {}", source: "Tyler Mcginnis", tests: []}
+        { id: "19078", description: "Rewrite reduce function.", initialCode: "function reduce() {}", source: "Tyler Mcginnis", tests: [] }
       ],
       draftCode: '',
       challengesCompleted: []
     }
   }
 
-  handleCodeChange(value) {
-    this.setState({ draftCode: value})
+  handleCodeChange(editor, data, value) {
+    this.setState({ draftCode: value })
   }
 
   verifyChallenge() {
+    try {
+      const fn = eval("(" + this.state.draftCode + ")") //disable
+      alert(fn(2, 1))
+
+    } catch (error) {
+      alert(error)
+    }
+
+
 
   }
 
@@ -34,21 +51,22 @@ class ChallengeContainer extends React.Component {
   currentChallenge(index = 0) {
     const currentChallenge = this.state.challenges[index]
     return <Challenge handleCodeChange={this.handleCodeChange.bind(this)}
-                      description={currentChallenge.description}
-                      initialCode={currentChallenge.initialCode}
-                      verifyChallenge={this.verifyChallenge.bind(this)}
-                      source={currentChallenge.source}/>;
+      description={currentChallenge.description}
+      initialCode={currentChallenge.initialCode}
+      verifyChallenge={this.verifyChallenge.bind(this)}
+      source={currentChallenge.source} />;
   }
 
   render() {
     const currentChallenge = this.currentChallenge()
 
     return (
-      <div>
-        {
-          currentChallenge
-        }
-      </div>
+      <iframe
+        src="https://codesandbox.io/embed/new?codemirror=1&moduleview=1&view=editor&hidenavigation=1&previewwindow=tests"
+        title="new"
+        style={style}
+        sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin">
+      </iframe>
     )
   }
 
