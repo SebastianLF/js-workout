251f0657539fb39719a6639a8e645744f19f605c
SebastianLF <slf.pro@gmail.com>
2019-10-13 22:33:37 +0200

    import esprima on app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Focus from './Focus.js';
 import ChallengeContainer from './ChallengeContainer.js';
+import esprima from 'esprima'
 
 // import logo from './logo.svg';
 import './App.css';
